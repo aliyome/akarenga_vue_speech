bac4f5b433a5cc135345e9844774136ed3d82d96
aliyome <aliyome@gmail.com>
2020-02-20 12:59:53 +0900

    feat: 雑なモックアップを作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div>No. {{ currentTranscript.no }}</div>
+    <div>日本語 {{ currentTranscript.ja }}</div>
+    <div>英語 {{ currentTranscript.en }}</div>
+    <textarea />
+    <div>
+      <span>{{ inputText }}</span>
+    </div>
+    <div>
+      <span style="color: red;">※ここに間違った部分を表示する</span>
+    </div>
+    <button @click="goNext">次へ</button>
+  </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import Vue from 'vue';
 
-export default {
+export interface HomeViewModel {
+  currentTranscript: Transcript;
+  inputText: string;
+}
+
+export interface Transcript {
+  no: number;
+  ja: string;
+  en: string;
+}
+
+export default Vue.extend({
   name: 'Home',
+  data(): HomeViewModel {
+    return {
+      currentTranscript: {
+        no: 0,
+        ja: '日本語の文章',
+        en: 'Japanese sentences',
+      },
+      inputText: 'Japanese sentence',
+    };
+  },
   components: {},
-};
+
+  methods: {
+    goNext() {
+      console.log('次へ');
+    },
+  },
+});
 </script>
